8b2ff0f271dab309a2656045e02bf1e87946467d
milo <milogreenmilo@gmail.com>
2022-04-24 10:43:51 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<h1>Milo Green</h1>
+				<nav>
+					<Link to="/">Home</Link>
+					<Link to="/projects">Projects</Link>
+					<Link to="/resume">Resume</Link>
+					<Link to="/contact">Contact</Link>
+				</nav>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
+import { 
+	HashRouter,
+	Routes,
+	Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import Projects from "./routes/projects";
+import Resume from "./routes/resume";
+import Contact from "./routes/contact";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById('root');
+render(
+	<HashRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/resume" element={<Resume />} />
+			<Route path="/contact" element={<Contact />} />
+		</Routes>
+	</HashRouter>,
+	rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
